3acba45303886fc1babed1a35230466e512e3b3b
Adewale Stephen <adewalestephennifemi@gmail.com>
2024-08-24 23:03:03 +0100

    added an util js file

---
+++ src/util.js
@@ -0,0 +1,9 @@
+export const cardInfo = {
+  heading: "HTML & CSS foundations",
+  cardText:
+    "These languages are the backbone of every website, defining structure, content, and presentation",
+  date: "Publish 21 Dec 2023",
+  button: "Learning",
+  btnLink: "#",
+  name: "Grey Hooper",
+};
