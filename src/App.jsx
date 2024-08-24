49611ceae09efaa922b70d2d1f379e5957529148
Adewale Stephen <adewalestephennifemi@gmail.com>
2024-08-24 23:03:36 +0100

    add: update added

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,20 @@
 import avatar from "./assets/image-avatar.webp";
 import article from "./assets/illustration-article.svg";
 import "./App.css";
+import { cardInfo } from "./util";
 export default function App() {
-  const cardInfo = {
-    heading: "HTML & CSS foundations",
-    cardText:
-      "These languages are the backbone of every website, defining structure, content, and presentation",
-    date: "Publish 21 Dec 2023",
-    button: "Learning",
-    btnLink: "#",
-    name: "Grey Hooper",
-  };
   return (
     <main>
       <img src={article} className="illustration" alt="article illustration" />
       <a href={cardInfo.btnLink}>
-        <button>{cardInfo.button}</button>
+        <button>{cardInfo.button || "Learning"}</button>
       </a>
-      <p>{cardInfo.date}</p>
+      <p>{cardInfo.date || "Publish 4 March 2024"}</p>
       <h1>{cardInfo.heading}</h1>
       <p>{cardInfo.cardText}</p>
       <div className="d-flex">
         <img src={avatar} alt="avater" />
-        <p>{cardInfo.name}</p>
+        <p>{cardInfo.name || "Adewale Stephen"}</p>
       </div>
     </main>
   );
